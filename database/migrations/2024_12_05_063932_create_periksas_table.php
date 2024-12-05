c19b081b973fdf065afbc53849238f92c0c185d8
raihanfaiq72 <faiq@gmail.com>
2024-12-05 14:04:18 +0700

    new app

---
+++ database/migrations/2024_12_05_063932_create_periksas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('periksas', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_daftar_poli');
+            $table->date('tanggal_periksa');
+            $table->string('catatan');
+            $table->string('biaya_periksa');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('periksas');
+    }
+};
